6763741e7056eeed047d65e89b161ba0071300db
Daniel Aquilino <prowebmaking@outlook.es>
2021-10-19 23:23:09 +0100

    Add files via upload

---
+++ paginaPrincipal.php
@@ -0,0 +1,158 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="editorPagina.css">
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <link rel="stylesheet" href="../css/bootstrap-utilities.min.css">
+    <title>Gol Factory</title>
+</head>
+<body>
+    <header>
+
+       <div class="barra-superior d-flex justify-content-between align-items-center" style="height:60px;">
+
+        <div class="ms-3">
+          <span style="color: #fff;">LOGO</span>
+        </div>
+            <div class="d-flex align-items-center">
+                <ul  class="submenu d-flex">
+                  <li>
+                      <a href="https://www.joma-sport.com">JOMA</a>
+                  </li>
+                  <li>
+                      <a href="https://www.joma-sport.com">ARTICULOS</a>
+                  </li>
+                  <li>
+                      <a href="https://www.instagrama.com/golfactory">CONTACTANOS</a>
+                  </li>
+                  <li>
+                      <a href="inicio.php">MI CUENTA</a>
+                  </li>
+
+                  </ul>
+          </div>
+
+       </div>
+    </header>
+
+    <div class="menu-desplegable d-flex justify-content-center align-items-content">
+      <div class="submenu-desplegable">
+
+        <a href="#">Futbol</a>
+
+        <a href="#">Baloncesto</a>
+
+        <a href="#">Tenis</a>
+
+        <a href="#">Ciclismo</a>
+
+        <a href="#">Atletismo</a>
+
+        <a href="#">Varios</a>
+      </div>
+      </div >
+
+  <div class="img-encabezado" style="height:500px; width:100%; ">
+
+          <div class="joma-logo">
+               <span>JOMA</span>
+          </div>
+
+          <div class="joma-info pb-5">
+                  <div class="row titulo-info" style="backgroung: brown; color: white">
+                      <h3>BIENVENIDO A GEPEGOLFAC</h3>
+                  </div>
+                  <div class="row  titulo-descripcion pt-4 " style="backgroung: brown; color: white">
+                      <div class="d-flex">
+                          <div class="info-descripcion">
+                            <span>Lorem ipsum dolor, sit amet consectetur <br> adipisicing elit. Quod reprehenderit </span>
+                          </div>
+                          <div class="boton-descripcion ms-4 d-flex align-items-end">
+                              <button class="btn btn-success">ENCARGAR</button>
+                          </div>
+                    </div>
+                  </div>
+           </div>
+           <div class="joma-caroussel d-flex justify-items-around">
+            <div class="img-joma-caroussel "><img src="imagen/pantalon2.jpg" alt=""></div>
+            <div class="img-joma-caroussel mx-3"><img src="imagen/chica2.jpg" alt=""></div>
+            <div class="img-joma-caroussel "><img src="imagen/camisas.jpg" alt=""></div>
+            <div class="img-joma-caroussel mx-3"><img src="imagen/chica1.jpg" alt=""></div>
+            <div class="img-joma-caroussel "><img src="imagen/zapatos1.jpg" alt=""></div>
+            </div>
+    </div>
+
+   <br><br>
+
+
+
+    <div class="container-principal d-flex">
+
+      <div class="barra-izquierda" style="background: green"> BARRA IZQUIERDA
+
+      </div>
+
+        <div class="container mx-3" style="background: yellow">
+            <h1>BIENVENIDO A GEPEGOLF</h1>
+            CONTENIDO PRINCIPAL
+        </div>
+
+        <div>
+        <div class="barra-derecha1" style="background: blue">BARRA DERECHA
+
+        </div>
+
+
+        <div class="barra-derecha2 mt-4 mb-4" style="background: pink">BARRA DERECHA
+
+        </div>
+
+        <div class="barra-derecha3" style="background: red">BARRA DERECHA
+
+        </div>
+       </div>
+
+
+    </div>
+
+ <div class="pie-pagina ">
+      <div class="pie-pagina-contenido d-flex justify-content-center">
+            <div class="contenido-footer-info">
+              <h5>Mas info</h5>
+              <hr>
+              <div>
+                <h6>Acerca de nosotros</h6>
+                <h6>Marcas</h6>
+                <h6>Tiendas</h6>
+              </div>
+            </div>
+            <div class="contenido-footer-catalogo mr-5">
+              <h5>Catalogo</h5>
+              <hr style="width:120px">
+            </div>
+            <div class="contenido-footer-contactos">
+              <h5>Contactos</h5>
+              <hr>
+            </div>
+      </div>
+     <div class="redes-sociales d-flex justify-content-center">
+
+       <div class="mx-3"> <h6>Facebook </h6></div>
+        <div styles="mx-5"> <h6>Twitter</h6></div>
+         <h6 class="mx-3">Instagram</h6>
+         <h6 style="">Whatsapp</h6>
+
+      </div>
+      <div class="d-r row d-flex justify-content-center">
+            <div style="width:500px">
+            <span>Copyright 2021 Todos los derechos reservados para Daniel</span>
+            </div>
+      </div>
+ </div>
+</body>
+
+<script src="js/bootstrap.min.js"></script>
+<script src="js/bootstrap.bundle.js"></script>
+</html>
